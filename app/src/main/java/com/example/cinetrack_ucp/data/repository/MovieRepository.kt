2576341770dd14b2e506c3b7a9d298accb9dc526
HafizZada16 <hafizzada16@gmail.com>
2026-01-11 18:30:08 +0700

    Update MovieRepository.kt

--- app/src/main/java/com/example/cinetrack_ucp/data/repository/MovieRepository.kt
+++ app/src/main/java/com/example/cinetrack_ucp/data/repository/MovieRepository.kt
@@ -1,12 +1,27 @@
 package com.example.cinetrack_ucp.data.repository
 
-import com.example.cinetrack_ucp.data.api.RetrofitClient
+import com.example.cinetrack_ucp.data.api.TmdbApiService
+import com.example.cinetrack_ucp.data.local.MovieDao
+import com.example.cinetrack_ucp.data.local.MovieEntity
 import com.example.cinetrack_ucp.model.MovieResponse
+import kotlinx.coroutines.flow.Flow
 import retrofit2.Response
 
-class MovieRepository {
-    // Fungsi untuk memanggil API
+class MovieRepository(
+    private val apiService: TmdbApiService,
+    private val movieDao: MovieDao
+) {
+    // Fungsi API
     suspend fun getPopularMovies(apiKey: String): Response<MovieResponse> {
-        return RetrofitClient.instance.getPopularMovies(apiKey)
+        return apiService.getPopularMovies(apiKey)
     }
+
+    // Fungsi Room (Local)
+    fun getAllFavorites(): Flow<List<MovieEntity>> = movieDao.getAllFavorites()
+
+    suspend fun insertFavorite(movie: MovieEntity) = movieDao.insertFavorite(movie)
+
+    suspend fun deleteFavorite(movie: MovieEntity) = movieDao.deleteFavorite(movie)
+
+    suspend fun isFavorite(id: Int): Boolean = movieDao.isFavorite(id)
 }
